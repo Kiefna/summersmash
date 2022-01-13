299cdb7bfad22fd9c3589d5364b7789bab004ba8
Kiefna <chris.candell@chatterblock.com>
2022-01-12 16:52:53 -0800

    fix for gunicron setting issue"

--- longnosebros/wsgi.py
+++ longnosebros/wsgi.py
@@ -11,6 +11,9 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'longnosebros.settings')
+try:
+    os.environ['DJANGO_SETTINGS_MODULE']
+except KeyError:
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'longnosebros.settings.production')
 
 application = get_wsgi_application()

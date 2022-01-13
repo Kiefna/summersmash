d5250c0c97f2a6d588447aeb0e76a839bc7ad435
Kiefna <chris.candell@chatterblock.com>
2022-01-12 16:55:47 -0800

    Attempt to comment out some code to fix setting issue

--- longnosebros/wsgi.py
+++ longnosebros/wsgi.py
@@ -11,9 +11,9 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-try:
-    os.environ['DJANGO_SETTINGS_MODULE']
-except KeyError:
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'longnosebros.settings.production')
+# try:
+#     os.environ['DJANGO_SETTINGS_MODULE']
+# except KeyError:
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'longnosebros.settings.production')
 
 application = get_wsgi_application()
